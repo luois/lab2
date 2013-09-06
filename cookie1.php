dd74314a5c24cb83be4004e55aea7f66710ebb6d
luois <lpink_11@hotmail.com>
2013-09-06 16:18:32 -0400

    New changelist

--- cookie1.php
+++ cookie1.php
@@ -1,20 +1,37 @@
-/**
- * Created by JetBrains PhpStorm.
- * User: root
- * Date: 13-09-03
- * Time: 15:03
- * To change this template use File | Settings | File Templates.
- */
-<?php
-if(!isset($_COOKIE['ybet']))
-{
-    Setcookie('ybet','site');
-    Print ("Première visite <br>");
-}else{
-    Print ("Vous avez déjà visité la page<br>");
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<head><h2>Voici la fonctionnalité cookie</h2></head>
+<boby>
+    <?php
+    if(!isset($_COOKIE['ybet']))
+    {
+        setcookie('ybet','site',time()+3600);
+        Print ("Première visite <br>");
+    }else{
+        Print ("Vous avez déjà visité la page<br>");
 
-    echo $_COOKIE['ybet']."<br>";
-}
-?>
+        echo $_COOKIE['ybet']."<br>";
+    }
+    ?>
+    <?php
+    if (ISSET($_POST['login'])
+    {
+        // boucle qui détecte la demande de connexion, vérifie les données de l'utilisateur et crée le cookie si les codes sont corrects
+    } ?>
 
-<h1>salut cookie page</h1>
+    <head>
+        <!-- contenu du header -->
+    </head>
+
+    <body>
+    // le contenu affiché
+
+    <form>
+        // le formulaire de connexion (login)
+    </form>
+
+    // suite du contenu affiché
+
+    </body>
+
+</boby>
+
